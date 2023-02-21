0ef808338d0a180f45167a0841ec6733e079f8b3
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-21 23:17:04 +0700

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -111,7 +111,7 @@ if ENV:
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
     WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
+    BAN_STICKER = os.environ.get("BAN_STICKER", "CAACAgUAAx0CTI8VjwACDe1j9O4UYpMelUGeOqUK4XhpSgIS9QACrAUAArpuaFeg87Tn0P08jS4E")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
